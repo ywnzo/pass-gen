c5144afaceb7c4dacadc823edb4ded30bdae32e7
Ernest Tamas <108870057+ywnzo@users.noreply.github.com>
2025-05-14 14:49:33 +0200

    Changed generator.js

--- generator.js
+++ generator.js
@@ -6,18 +6,50 @@ const copied_alert = document.querySelector('#copied-alert');
 
 class PasswordGenerator {
     chars = "qwertyuopasdfghkzxcvbnmQWERTYUOPASDFGHKZXCVBNM1234567890";
+    capitals = "QWERTYUOPASDFGHKZXCVBNM";
+    numbers = "1234567890";
 
-    get_random() {
-        return this.chars[Math.floor(Math.random() * this.chars.length)];
+    get_random(arr) {
+        return arr[Math.floor(Math.random() * arr.length)];
     }
     
     generate() {
         var amount = char_amount.value;
         var password = '';
+
+        var hasNumbers = false;
+        var hasCapital = false;
         
         for (let i = 0; i < amount; i++) {
-            const char = this.get_random();
+            const char = this.get_random(this.chars);
             password += char;
+        }
+
+        for (let i = 0; i < password.length; i++) {
+            if(this.capitals.includes(password[i])) {
+                hasCapital = true;
+            }
+            if(this.numbers.includes(password[i])) {
+                hasNumbers = true;
+            }
+        }
+
+        if(!hasCapital) {
+            for (let i = 0; i < password.length; i++) {
+                if(!this.numbers.includes(password[i])) {
+                    password = password.replace(password[1], this.get_random(this.capitals))
+                    break;
+                }
+            }
+        }
+
+        if(!hasNumbers) {
+            for (let i = 0; i < password.length; i++) {
+                if(!this.capitals.includes(password[i])) {
+                    password = password.replace(password[1], this.get_random(this.numbers))
+                    break;
+                }
+            }
         }
         
         password_output.style.scale = '0.9';
